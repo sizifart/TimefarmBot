d0abe2fb2d662c9fe52356847ec0ea8279963d70
Foad Davoodi <myf.davoodi@gmail.com>
2024-09-08 21:31:42 +0330

    Add files via upload

---
+++ bot/config/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+    CLAIM_RETRY: int = 1
+    SLEEP_BETWEEN_CLAIM: int = 240
+
+    AUTO_UPGRADE_FARM: bool = True
+    MAX_UPGRADE_LEVEL: int = 6
+
+    USE_PROXY_FROM_FILE: bool = False
+
+
+settings = Settings()
